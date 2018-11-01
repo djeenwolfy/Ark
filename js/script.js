c9849c0741f7c5fe62576cd19fb36cd78eb19990
djeenwolfy <volkovpavelvladislavovich@gmail.com>
2018-11-01 17:48:17 +0300

    Ark 01112018

---
+++ js/script.js
@@ -0,0 +1,9 @@
+$(document).ready(function () {
+ $("#comments-slider").owlCarousel({
+ navigation: false,
+ slideSpeed: 100,
+ paginationSpeed: 800,
+ singleItem: true,
+ autoPlay: true
+ });
+ });
